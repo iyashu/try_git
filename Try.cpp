561b68ef8e0b39c699edb51908872cc211dae6b5
Yashpal Choudhary <yashpal.c1995@gmail.com>
2013-12-05 00:55:13 +0530

    Added a new feature in try.cpp

--- Try.cpp
+++ Try.cpp
@@ -4,5 +4,7 @@ int main()
 {
 	cout<<"Hey I have modified the code now "<<endl;
 	cout<<"See what happens now"<<endl;
+
+	cout<<"I have added one more line to make a new feature..:)"<<endl;
 	return 0;
 }
