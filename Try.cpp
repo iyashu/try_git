e822af6710a5063d8e91f71dfd72ad06a2986476
Yashpal Choudhary <yashpal.c1995@gmail.com>
2013-12-05 01:08:26 +0530

    Added another file

--- Try.cpp
+++ Try.cpp
@@ -5,6 +5,6 @@ int main()
 	cout<<"Hey I have modified the code now "<<endl;
 	cout<<"See what happens now"<<endl;
 
-	cout<<"I have added one more line to make a new feature..:)"<<endl;
+	cout<<"I have added one more line to make a new feature...:)"<<endl;
 	return 0;
 }
